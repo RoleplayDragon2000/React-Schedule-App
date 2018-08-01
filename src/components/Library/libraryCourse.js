411e9657ec512b042084ed4e7c19d34998c54864
Michelle Dickson <19mdickson@go.dsdmail.net>
2018-08-01 12:00:56 -0600

    Added Fontawesome Plus Icon

--- src/components/Library/libraryCourse.js
+++ src/components/Library/libraryCourse.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Icon from "../icon";
 import Arrow from "../arrow";
+import Action from "../action"
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-              <div className="library-course__title-arrow"> 
+              <div className="library-course__title-check"> 
               <label className="library-course__title">Dragons Through the Ages</label>
                 { Icon('fas fa-check', 'library-course__icon')}
              </div>
                 <Arrow className="library-course__arrow"/>
-                {/* button */}
+                <Action className="library-course__action"/>
             <div className="library-course__description"></div>
                 <label>Course Description</label>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sapien luctus, pretium magna nec, commodo magna. Nullam rutrum maximus pulvinar. Aliquam eu eleifend tortor. Mauris dapibus, sapien eget posuere fermentum, nisl velit egestas tortor, eget tincidunt velit mauris eu nunc. </p>
--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Icon from "./icon";
 
 class Arrow extends Component {
     render() {
         return(
-            <div className={`${this.props.className} arrow`}></div>
+            <div className={`${this.props.className} arrow`}>
+            {Icon("fas fa-plus-circle")}
+            {/* fas fa-times-circle */}
+            </div>
         )
     }
 }
