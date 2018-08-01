15f3b357f512b1587d24f4e9f4e89952fa8b882a
Michelle Dickson <19mdickson@go.dsdmail.net>
2018-08-01 08:53:20 -0600

    Initial Commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Schedule from "./schedule/schedule";
+import Library from "./library/library";
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <h1>Course Scheduler</h1>
+        <Library/>
+        <Schedule/>
       </div>
     );
   }
