ca7021809e96b1b2576815015055de09c475ec05
Michelle Dickson <19mdickson@go.dsdmail.net>
2018-08-07 09:40:48 -0600

    Added in custom info functionality

--- src/components/Library/libraryCourse.js
+++ src/components/Library/libraryCourse.js
@@ -8,13 +8,13 @@ class LibraryCourse extends Component {
         return (
             <div className="library-course">
               <div className="library-course__title-check"> 
-              <label className="library-course__title">Dragons Through the Ages</label>
+              <label className="library-course__title">{this.props.title}</label>
                 { Icon('fas fa-check', 'library-course__icon')}
              </div>
                 <Arrow className="library-course__arrow"/>
                 <Action className="library-course__action"/>
             <div className="library-course__description"></div>
-                <label>Course Description</label>
+            <label>{this.props.description}</label>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sapien luctus, pretium magna nec, commodo magna. Nullam rutrum maximus pulvinar. Aliquam eu eleifend tortor. Mauris dapibus, sapien eget posuere fermentum, nisl velit egestas tortor, eget tincidunt velit mauris eu nunc.
                 </p>
