3c74f581e1c02eba38e3d52d1b456d803afc9bad
Michelle Dickson <19mdickson@go.dsdmail.net>
2018-08-08 08:13:05 -0600

    fixed onclick bug

--- src/components/action.js
+++ src/components/action.js
@@ -4,7 +4,7 @@ import Icon from './icon'
 class Action extends Component {
     render() {
         return(
-            <button onClick={this.props.onClick()} className={`${this.props.className} action`}>
+            <button onClick={this.props.onClick} className={`${this.props.className} action`}>
                 {Icon('fas fa-plus-circle')}
             </button>
         )
